1f96992fc63263323a0d2e4c762e40022c97505e
Eleglac <happy.ajp050789@gmail.com>
2016-08-15 20:14:46 -0700

    Cleaning up code

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject tri-vis "0.1-alpha"
 
-  :description "Inspired by a Macbook cover."
+  :description "Tri-Vis: Inspired by a Macbook cover."
 
   :url "http://pontingdynamics.org/tri-vis"
 
-  :license {:name "Not A Clue License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU GPL v3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [trystan/delaunay-triangulation "1.0.1"]
